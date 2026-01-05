bcc78249ea66c88276a087b83c063ee91283ee92
ayush-that <ayush1337@hotmail.com>
2026-01-05 20:23:57 +0530

    Initialize: Simple Analytics Raycast extension

---
+++ raycast-env.d.ts
@@ -0,0 +1,32 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Menu Bar Display - What statistics to show in the menu bar */
+  displayMode: "visitors" | "pageviews" | "both";
+  /** Default Time Range - Default time range for statistics */
+  defaultTimeRange: "today" | "7d" | "30d";
+};
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences;
+
+declare namespace Preferences {
+  /** Preferences accessible in the `menu-bar-stats` command */
+  export type MenuBarStats = ExtensionPreferences & {};
+  /** Preferences accessible in the `manage-websites` command */
+  export type ManageWebsites = ExtensionPreferences & {};
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `menu-bar-stats` command */
+  export type MenuBarStats = {};
+  /** Arguments passed to the `manage-websites` command */
+  export type ManageWebsites = {};
+}
